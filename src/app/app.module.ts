ab4188496fcb97137c5af5442b71f73c318546f2
ozymandias54 <bassirou718@gmail.com>
2024-06-19 12:16:27 +0000

    quizz page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './pages/quizz/quizz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    QuizzComponent
   ],
   imports: [
     BrowserModule,
